aa3d1756acd7edf7e2e62367a7d9dfe9dcebfc7b
ilias bhallil <ilias.bhal@gmail.com>
2022-08-29 21:23:36 +0300

    add ability to specify input and output paths

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,29 @@
 #! /usr/bin/env node
 
 import { tsnode } from './codegen/tsnode';
+import { program } from 'commander';
+import fs from 'fs';
+import path from 'path';
 
-const entryFile = './index';
-const outputFileName = 'generated.ts';
+const getPkgInfo = () => {
+  const pkgjsonPath = path.resolve(__dirname, '..', 'package.json');
+  const pkgInfoString = fs.readFileSync(pkgjsonPath, 'utf-8');
+  const pkgInfo = JSON.parse(pkgInfoString);
+  return pkgInfo;
+}
+
+const pkgInfo = getPkgInfo();
+
+program.name(pkgInfo.name)
+  .version(pkgInfo.version)
+  .option('-i --input <path>', 'input file path')
+  .option('-i --output <filename>', 'desired generated file name');
+
+program.parse();
+
+const options = program.opts();
+const entryFile = options.input || './index';
+const outputFileName = options.output + '.ts' || 'generated.ts';
 
 // We need to run the script with ts-node 
 // to be able to import entry ts file
